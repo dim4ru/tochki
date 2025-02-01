0cd1903848adf744ff5cb814ed314cfe3aa2700f
dim4 <@>
2025-02-01 14:11:30 +0600

    user location, location permissions

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,33 +15,31 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Tochki',
       home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Color(0xffFF5600),
-            title: Text("Tochki"),
+        appBar: AppBar(
+          backgroundColor: Color(0xffFF5600),
+          title: Text("Tochki"),
+        ),
+        body: FlutterMap(
+          options: MapOptions(
+            initialCenter: LatLng(54.989221, 73.368456),
+            // Center the map over London
+            initialZoom: 15,
           ),
-          body: FlutterMap(
-            options: MapOptions(
-              initialCenter: LatLng(54.989221, 73.368456),
-              // Center the map over London
-              initialZoom: 15,
+          children: [
+            TileLayer(
+              // Display map tiles from any source
+              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+              // OSMF's Tile Server
+              userAgentPackageName: 'com.example.app',
+              // And many more recommended properties!
             ),
-            children: [
-              TileLayer( // Display map tiles from any source
-                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                // OSMF's Tile Server
-                userAgentPackageName: 'com.example.app',
-                // And many more recommended properties!
-              ),
-              RichAttributionWidget( // Include a stylish prebuilt attribution widget that meets all requirments
-                attributions: [
-                  TextSourceAttribution(
-                    'OpenStreetMap contributors',
-                  ),
-                  // Also add images...
-                ],
-              ),
-            ],
-          ),
+            CurrentLocationLayer(),
+            FloatingActionButton(
+              onPressed: () {},
+
+            )
+          ],
+        ),
       ),
     );
   }
