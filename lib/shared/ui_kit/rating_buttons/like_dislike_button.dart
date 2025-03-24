ddfb984a269356c3df3bfc265876448b8b2e8623
dim4 <dmitry.vlasov@softdev.pw>
2025-03-24 15:34:50 +0600

    feat: like dislike button & fixed depricated

--- lib/shared/ui_kit/rating_buttons/like_dislike_button.dart
+++ lib/shared/ui_kit/rating_buttons/like_dislike_button.dart
@@ -11,35 +11,23 @@ class LikeDislikeButton extends StatelessWidget {
       children: [
         Expanded(
           child: UiButton.filledPrimary(
-            iconAlignment: IconAlignment.end,
             icon: Icon(CupertinoIcons.plus),
             onPressed: () {},
             style: ButtonStyle(
-              // TODO refactor
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              shape: WidgetStateProperty.all<RoundedRectangleBorder>(
                   RoundedRectangleBorder(
                     borderRadius: BorderRadius.only(topLeft: Radius.circular(8), bottomLeft: Radius.circular(8), topRight: Radius.zero, bottomRight: Radius.zero),
                   )),
             ),
           ),
         ),
-        // SizedBox(
-        //   width: TSpacers.spacing1,
-        // ),
-        // Container(
-        //   // color: TColors.black,
-        //   child: Column(
-        //     children: [
-        //       Text('Рейтинг\n 68%', style: TTypography.caption1, textAlign: TextAlign.center,)
-        //     ],
-        //   ),
-        // ),
         UiButton.filledPrimary(
-          iconAlignment: IconAlignment.end,
-          label: Text('68%', style: TTypography.caption1, textAlign: TextAlign.center,),
+          iconAlignment: IconAlignment.start,
+          icon: Icon(Icons.thumbs_up_down_outlined),
+          label: Text('68%', style: TTypography.body3, textAlign: TextAlign.center,),
           onPressed: () {},
           style: ButtonStyle(
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            shape: WidgetStateProperty.all<RoundedRectangleBorder>(
                 RoundedRectangleBorder(
                   borderRadius: BorderRadius.all(Radius.zero),
                 )),
@@ -47,14 +35,13 @@ class LikeDislikeButton extends StatelessWidget {
         ),
         Expanded(
           child: UiButton.filledPrimary(
-            iconAlignment: IconAlignment.end,
             icon: Icon(CupertinoIcons.minus),
             onPressed: () {},
             style: ButtonStyle(
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              shape: WidgetStateProperty.all<RoundedRectangleBorder>(
                   RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(topLeft: Radius.zero, bottomLeft: Radius.zero, topRight: Radius.circular(8), bottomRight: Radius.circular(8)),
-              )),
+                    borderRadius: BorderRadius.only(topLeft: Radius.zero, bottomLeft: Radius.zero, topRight: Radius.circular(8), bottomRight: Radius.circular(8)),
+                  )),
             ),
           ),
         ),
